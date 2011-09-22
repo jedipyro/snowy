8df6661059edfb8973a638b67437e943a06e895a
Leho Kraav <leho@kraav.com>
2011-09-22 20:21:36 +0300

    Django 1.3 compatibility update (partial)

--- settings.py
+++ settings.py
@@ -18,12 +18,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': '',          # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',            # Or path to database file if using sqlite3.
+        'USER': '',            # Not used with sqlite3.
+        'PASSWORD': '',        # Not used with sqlite3.
+        'HOST': '',            # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': ''             # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 RECAPTCHA_ENABLED = False
 RECAPTCHA_PUBLIC_KEY = ''
@@ -73,13 +77,13 @@ SECRET_KEY = '2n17f16%vd2^x=q6&7a)s*$ft@vtp572-4+)@6spfxpe^^7^j6'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
-    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
