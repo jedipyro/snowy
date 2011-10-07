85ec20e16a0c08f64bc30421c04dc76b0c0e971b
Jeff Schroeder <jeffschroeder@computer.org>
2011-10-06 20:30:05 -0700

    Update settings.py to use the new csrf middleware

--- settings.py
+++ settings.py
@@ -94,8 +94,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.csrf.middleware.CsrfViewMiddleware',
-    'django.contrib.csrf.middleware.CsrfResponseMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
